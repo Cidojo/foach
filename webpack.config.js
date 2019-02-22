bb5130281218b9be5a06bd7134fbc5912dc9d825
Cidojo <cidell@mail.ru>
2019-02-22 14:10:51 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'js/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    port: 3000,
+    publicPath: '/'
+  },
+  devtool: 'source-map',
+  plugins: [
+    new CopyPlugin([
+      {
+        from: 'src/img/',
+        to: 'img'
+      },
+      {
+        from: 'src/fonts/',
+        to: 'fonts'
+      }
+    ]),
+    new ImageminPlugin([{
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      disable: process.env.NODE_ENV !== 'production', // Disable during development
+      pngquant: {
+        quality: '95-100'
+      }
+    }])
+  ]
+}
